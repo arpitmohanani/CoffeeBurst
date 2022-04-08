2c4eddaafea86d6bfd1954cbafc77aed6d0c8fa2
vorobyovayana <vorobyovayana@yandex.ru>
2022-04-07 19:47:33 -0700

    "Added radiobuttons, fixed issues with toolbar"

--- app/src/main/java/com/example/rocketfuel/activities/ProductCardActivity.java
+++ app/src/main/java/com/example/rocketfuel/activities/ProductCardActivity.java
@@ -2,12 +2,16 @@
 package com.example.rocketfuel.activities;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.rocketfuel.R;
@@ -15,38 +19,61 @@ import com.example.rocketfuel.R;
 import java.util.Objects;
 
 public class ProductCardActivity extends AppCompatActivity {
+    String productName ="";
+    String productPrice ="";
+    String productImage ="";
+    TextView drinkName;
+    ImageView drinkImage;
+    TextView drinkPrice;
+    RadioGroup radioGroupSizes;
+    String productPriceStr;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_card_layout);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        drinkName = findViewById(R.id.drink_name);
+        drinkImage = findViewById(R.id.imgViewProductCardImage);
+        drinkPrice = findViewById(R.id.txtViewPrice);
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-//        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.cappuchino));
-//        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//            }
-//        });
-
         Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
-        String imgId = intent.getStringExtra("imgId");
+        productName = intent.getStringExtra("name");
+        productImage = intent.getStringExtra("imgId");
+        productPrice = intent.getStringExtra("price");
+        productPrice = intent.getStringExtra("price");
 
-
-
-        TextView drinkName = findViewById(R.id.drink_name);
-        ImageView drinkImage = findViewById(R.id.imgViewProductCardImage);
-        drinkName.setText(name);
+        drinkName.setText(productName);
+        drinkPrice.setText(productPriceStr);
+        //drinkImage.setImageDrawable();
 
     }
 
     public void onRadioButtonClicked(View view) {
+        radioGroupSizes = findViewById(R.id.radioGroupSizes);
+        int selectedId = radioGroupSizes.getCheckedRadioButtonId();
+        String selectedValue =  (String) ((RadioButton) findViewById(selectedId)).getText();
+        if(selectedValue.equalsIgnoreCase("Small")){
+            System.out.println(selectedValue);
+            drinkPrice.setText(productPrice);
+            System.out.println(productPrice);
+        }
+
+
+        else if(selectedValue.equalsIgnoreCase("Medium")){
+            System.out.println(selectedValue);
+            //drinkPrice.setText(String.valueOf(Double.parseDouble(productPrice)*1.5));
+            System.out.println(Double.valueOf(productPriceStr));
+        }
+
+        else if(selectedValue.equalsIgnoreCase("Large")){
+            System.out.println(selectedValue);
+            //drinkPrice.setText(String.valueOf(Double.parseDouble(productPrice)*2.0));
+            System.out.println(Double.valueOf(productPriceStr));
+        }
 
     }
+
 }
--- app/src/main/java/com/example/rocketfuel/adapters/ProductAdapter.java
+++ app/src/main/java/com/example/rocketfuel/adapters/ProductAdapter.java
@@ -1,5 +1,6 @@
 package com.example.rocketfuel.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.rocketfuel.model.Product;
@@ -38,8 +40,15 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.Viewhold
         // to set data to textview and imageview of each card layout
         Product model = productArrayList.get(position);
         holder.productNameTV.setText(model.getProduct_name());
-        holder.productPriceIV.setText("" + model.getProduct_price());
-        holder.productIV.setImageResource(context.getResources().getIdentifier(model.getProduct_image() , "drawable", context.getPackageName()));
+        holder.productPriceIV.setText("$" + model.getProduct_price());
+//        System.out.println(context.getResources().getIdentifier(model.getProduct_image().split("\\.")[0], "drawable", context.getPackageName()));
+        holder.productIV.setImageDrawable(AppCompatResources.getDrawable(context,context.getResources().getIdentifier(model.getProduct_image().split("\\.")[0] , "drawable", context.getPackageName())));
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void updateList(ArrayList<Product> productArrayList){
+        this.productArrayList = productArrayList;
+        notifyDataSetChanged();
     }
 
     @Override
